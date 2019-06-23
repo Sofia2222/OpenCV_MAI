ced1612368a9dd89913e9798ad08264c7179d6a4
kuzmina22 <kyana2016@mail.ru>
2019-06-23 22:12:48 +0300

    filter gui

--- opencv/src/main/java/ru/mai/opencv/Application.java
+++ opencv/src/main/java/ru/mai/opencv/Application.java
@@ -1,9 +1,25 @@
 package ru.mai.opencv;
 
 import nu.pattern.OpenCV;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class Application {
+
     public static void main(String[] args) {
         OpenCV.loadShared();
+
+        String filePath = "opencv/src/main/resources/cv.png";
+        Mat newImage = Imgcodecs.imread(filePath);
+
+        if (newImage.dataAddr() == 0) {
+            System.out.println("Couldn't open file " + filePath);
+        } else {
+            FilterGUI filterGui = new FilterGUI("Smooth Filter Example", newImage);
+            filterGui.init();
+           // MorphologyGUI morphologyGui = new MorphologyGUI("Morphology Example", newImage);
+           // morphologyGui.init();
+        }
     }
+
 }
